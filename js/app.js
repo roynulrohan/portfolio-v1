98e9bc1b6b9e3c73f0c52e0b10fc77a9bc5aa7ff
Roynul Rohan <43069448+roynulrohan@users.noreply.github.com>
2020-12-11 11:39:17 -0500

    Added proper about me section. Reduced skills to recent skills only

--- js/app.js
+++ js/app.js
@@ -9,3 +9,12 @@ $('.animate').hover(function () {
     $(this).addClass('animated');
 });
 
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($('nav').offset().top < 100) {
+            $('nav').removeClass('hidden');
+        } else {
+            $('nav').addClass('hidden');
+        }
+    });
+});
