eb2c23eb610667bc875273a1cd394cc7351f7ece
Roynul Rohan <43069448+roynulrohan@users.noreply.github.com>
2020-12-12 12:48:00 -0500

    Added buttons to navigate between sections.

--- js/app.js
+++ js/app.js
@@ -11,10 +11,46 @@ $('.animate').hover(function () {
 
 $(document).ready(function () {
     $(window).scroll(function () {
-        if ($('nav').offset().top < 100) {
+        if ($('nav').offset().top < 10) {
             $('nav').removeClass('hidden');
+            $('.previous').addClass('hidden');
         } else {
             $('nav').addClass('hidden');
+            $('.previous').removeClass('hidden');
         }
     });
+});
+
+sections = $('section');
+s = 0;
+
+$('.next').click(function () {
+    if (s < sections.length - 1) {
+        s++;
+        $('html, body').animate(
+            {
+                scrollTop: sections.eq(s).offset().top,
+            },
+            50
+        );
+    }
+});
+
+$('.previous').click(function () {
+    if (s > 0) {
+        s--;
+        $('html, body').animate(
+            {
+                scrollTop: sections.eq(s).offset().top,
+            },
+            50
+        );
+    } else {
+        $('html, body').animate(
+            {
+                scrollTop: 0,
+            },
+            50
+        );
+    }
 });
