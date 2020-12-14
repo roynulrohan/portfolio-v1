d387f32e155ba43e6c7e91acb32a1eb1f16839e3
Roynul Rohan <43069448+roynulrohan@users.noreply.github.com>
2020-12-14 11:43:58 -0500

    Added pages for projects. Added page animations. Fixed some paddings and margins. Adjusted slide up animations.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-let currentSection = '';
+let currentSection = ''; // current section hash
 
+// letters bounce animation
 $('.animate').bind(
     'webkitAnimationEnd mozAnimationEnd animationend',
     function () {
@@ -11,7 +12,42 @@ $('.animate').hover(function () {
     $(this).addClass('animated');
 });
 
+// on load
 $(document).ready(function () {
+    // on load repeats all functions for consistency between pages
+    if ($('nav').offset().top < 10) {
+        $('nav').removeClass('hidden');
+        $('.previous').addClass('hidden');
+    } else {
+        $('nav').addClass('hidden');
+        $('.previous').removeClass('hidden');
+    }
+
+    $('.content-panel').each(function () {
+        if (isVisible($(this))) {
+            $(this).removeClass('hidden');
+        } else {
+            $(this).addClass('hidden');
+        }
+    });
+
+    $('section').each(function () {
+        if (
+            $(this).offset().top < window.pageYOffset + 400 &&
+            //begins before top
+            $(this).offset().top + $(this).height() > window.pageYOffset + 400
+            //but ends in visible area
+        ) {
+            if ($(this).attr('id') != 'hero') {
+                currentSection = '#' + $(this).attr('id');
+            } else {
+                currentSection = '';
+            }
+        }
+    });
+
+    $('.wrapper').removeClass('hidden');
+
     $(window).scroll(function () {
         if ($('nav').offset().top < 10) {
             $('nav').removeClass('hidden');
@@ -23,6 +59,7 @@ $(document).ready(function () {
     });
 });
 
+// function to check if element is in view
 function isVisible(elem) {
     var docViewTop = $(window).scrollTop();
     var docViewBottom = docViewTop + $(window).height();
@@ -33,6 +70,7 @@ function isVisible(elem) {
     return elemBottom <= docViewBottom;
 }
 
+// content slide up animation
 $(window).scroll(function () {
     $('.content-panel').each(function () {
         if (isVisible($(this))) {
@@ -43,6 +81,7 @@ $(window).scroll(function () {
     });
 });
 
+// on scroll set currentSection hash to section in view
 $(document).bind('scroll', function (e) {
     $('section').each(function () {
         if (
@@ -60,10 +99,12 @@ $(document).bind('scroll', function (e) {
     });
 });
 
+// set hash on nav link click
 $('.nav-link').click(function () {
     currentSection = $(this).attr('href');
 });
 
+// section next button
 $('.next').click(function () {
     if (currentSection == '') {
         window.location.hash = 'about';
@@ -77,6 +118,7 @@ $('.next').click(function () {
     }
 });
 
+// section previous button
 $('.previous').click(function () {
     if (currentSection == '#contact') {
         window.location.hash = '#projects';
